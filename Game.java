e132655fb37313a7511a37ce1d65b870cd4325fb
Allan <alcollins23@gmail.com>
2013-11-17 17:43:25 -0330

    Update Game.java

--- Game.java
+++ Game.java
@@ -9,7 +9,7 @@ public class Game extends JFrame {
 	
 	public Game(){
 		this.setLayout(new BorderLayout());
-		setSize(750,700);
+		setSize(750,490);
 		this.setTitle("Stuff");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
