8aa6435ebbf93e4d7456ab1db24cbab6c9387975
Allan <alcollins23@gmail.com>
2013-11-17 11:46:59 -0330

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -1,13 +1,19 @@
-/**
- *
- *
- **/
-public class Tile{
-    String type;
-    String imageURL;
-    image Image;
+import java.awt.Image;
 
-    public image getImage(){
+import javax.swing.ImageIcon;
 
-    }
+
+public class Tile {
+	String type; //G - grass, H - hills, M - mountains, F - forest, W - water, I - Ice, etc.
+	String imageUrl = "Tile.png";
+	Image image;
+	
+	public Tile(String type){
+		this.type = type;
+		imageUrl = type + imageUrl; //concatenates ie. GTile.png, WTile.png, etc.
+		ImageIcon ii = new ImageIcon(this.getClass().getResource(imageUrl)); 
+		this.image = ii.getImage();
+	}
+	public Image getImage(){return this.image;}
+
 }
