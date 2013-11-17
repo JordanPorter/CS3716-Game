a48c1befc6518068dd24a93a985c3d39d5e0c74f
Allan <alcollins23@gmail.com>
2013-11-17 11:46:24 -0330

    Create Unit.java

---
+++ Unit.java
@@ -0,0 +1,21 @@
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Unit {
+	int row; //current tile row
+	int col; //current tile col
+	private Image image;
+	//private String unitPicUrl; //= "unit.png"
+
+	public Unit(int row, int col, String unitPicUrl){
+		this.row = row;
+		this.col = col;
+		//this.unitPicUrl = unitPicUrl;
+		ImageIcon ii = new ImageIcon(this.getClass().getResource(unitPicUrl));
+		image = ii.getImage();
+	}
+	public int getRow(){return this.row;}
+	public int getCol(){return this.col;}
+	public void move(int row,int col){this.row = row; this.col = col;}
+	public Image getImage(){return this.image;}
+}
