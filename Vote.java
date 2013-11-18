21bffd00d0194de49c0229296739fa837ad1851d
JordanPorter <jordanporter@n001h088.wst.mun.ca>
2013-11-18 15:21:51 -0330

    Changed the Vote class to an Interface to make it easier to make different kinds of votes
    Election will extend Vote
    Region will extend Vote (to vote to create a country)

--- Vote.java
+++ Vote.java
@@ -1,54 +1,16 @@
-import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 
 /**
  * @author Jordan Porter
  * 
- * The vote class creates a new vote and queries
- * players for a 'yay' or 'nay' vote. 
+ * The vote interface has methods for
+ * taking a vote and for creating a voting window
  *
  */
-public class Vote {
+public interface Vote {
 
-	private ArrayList<Unit> players;
-	
-	private ArrayList<Unit> yay;
-	private ArrayList<Unit> nay;
-	
-	/**
-	 * Constructs a new vote with a list of
-	 * relevant players
-	 * 
-	 * @param players
-	 */
-	public Vote(ArrayList<Unit> players)	{
-		this.players = players;
-	}
-	
-	/**
-	 * Takes a vote of all the players
-	 * calling a vote method in each Unit's
-	 * class which generates a pop up window
-	 * to cast their vote. Then calculates whether
-	 * the vote is successful or not.
-	 * 
-	 * @return true/false
-	 */
-	public boolean takeVote()	{
-		yay = new ArrayList<Unit>();
-		nay = new ArrayList<Unit>();
-		
-		for(Unit u : players)	{
-			if(u.vote() == true)
-				yay.add(u);
-			else
-				nay.add(u);
-		}
-		
-		if(yay.size() > nay.size())	
-			return true;
-		else
-			return false;
-	}
+	public boolean vote();
+	public JOptionPane voteWindow();
 	
 }
