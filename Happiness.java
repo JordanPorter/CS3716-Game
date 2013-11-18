e195fd5e45627a8a134b08cef817ab7a3e79a1d4
JordanPorter <jordanporter@LocalHUB.local>
2013-11-18 14:12:51 -0330

    Added my project to the gitHub

--- Happiness.java
+++ Happiness.java
@@ -5,7 +5,7 @@ import java.util.Map;
  **/
 public class Happiness{
     enum type{govern,money,leftAlone,building,killing};
-    Map map{type, int value}; // finish
+    //Map map = {type, value}; // finish
     int happinessCredits;
     // Calculates happiness depending on the type the player chose.
     public int calculateHappiness(){
