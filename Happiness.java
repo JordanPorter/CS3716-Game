4e36136d731d2d92889981ba4c848bb404e9e5b7
JKRodgers <rodgers_13@yahoo.ca>
2013-11-20 14:52:43 -0330

    Added more options for Happiness.

--- Happiness.java
+++ Happiness.java
@@ -5,31 +5,29 @@
  **/
 public class Happiness{
 	// enum type of each the different types of happiness in our game
-    enum HapType {GOVERN, MONEY, LEFT_ALONE, BUILDING, KILLING};
+    //enum HapType {GOVERN, MONEY, LEFT_ALONE, BUILDING, KILLING}; no longer using enum
+	// integer value for the ratio of each type of happiness
+    double govern;
+    double money;
+    double left_alone;
+    double building;
+    double killing;
     // type of happiness for the player associated
-    HapType type;
-    // amount of happiness
-    int happiness;
+    //HapType type;
+    // total amount of happiness
+    double happiness;
     // Constructor for happiness
-    public Happiness(HapType type){
-    	this.type = type;
+    public Happiness(double govern, double money, double left_alone, double building, double killing){
+    	this.govern = govern;
+    	this.money = money;
+    	this.left_alone = left_alone;
+    	this.building = building;
+    	this.killing = killing;
     	happiness = 0;
     }
     // Calculates happiness depending on the type the player chose at the beginning of the game
-    public int calculateHappiness(){
-        // switch case to calculate the happiness of this player based on chosen type
-    	switch(type){
-    		case GOVERN:
-    			break;
-    		case MONEY:
-    			break;
-    		case LEFT_ALONE:
-    			break;
-    		case BUILDING:
-    			break;
-    		case KILLING:
-    			break;
-    	}
+    public double calculateHappiness(){
+    	// takes the ratios of each happiness type and calculates the full value of happiness
         return happiness;
     }
     
--- Unit.java
+++ Unit.java
@@ -1,7 +1,12 @@
+import java.awt.GridLayout;
 import java.awt.Image;
+import java.text.NumberFormat;
 
 import javax.swing.ImageIcon;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 /**
  * @author 
@@ -44,26 +49,56 @@ public class Unit {
 		return this.image;
 	}
 	
+	/**
+	 * setHappiness will prompt the user to place its percent happiness values for a total of 100
+	 * points, if the user enters an invalid format or numbers not adding up to 100 the user will
+	 * be notified and prompted to correct their mistakes.
+	 * Happiness values will then be set.
+	 */
 	public void setHappiness(){
-		Object[] selection = {"GOVERN", "MONEY", "LEFT_ALONE", "BUILDING", "KILLING"};
-		String initial = "GOVERN";
-		String type = (String) JOptionPane.showInputDialog(null, "Choose your happiness type:", "Happiness Type", JOptionPane.QUESTION_MESSAGE, null, selection, initial);
-		switch(type){
-			case "GOVERN":
-				hap = new Happiness(Happiness.HapType.GOVERN);
-				break;
-			case "MONEY":
-				hap = new Happiness(Happiness.HapType.MONEY);
-				break;
-			case "LEFT_ALONE":
-				hap = new Happiness(Happiness.HapType.LEFT_ALONE);
-				break;
-			case "BUILDING":
-				hap = new Happiness(Happiness.HapType.BUILDING);
-				break;
-			case "KILLING":
-				hap = new Happiness(Happiness.HapType.KILLING);
-				break;
+		JPanel choices = new JPanel(new GridLayout(5,2));
+		double gValue = 0, mValue = 0, lValue = 0, bValue = 0, kValue = 0;
+		NumberFormat percentFormat = NumberFormat.getNumberInstance();
+		percentFormat.setMinimumFractionDigits(3);
+		JLabel governLabel = new JLabel("Govern(%):");
+		JFormattedTextField gNum = new JFormattedTextField();
+		JLabel moneyLabel = new JLabel("Money(%):");
+		JFormattedTextField mNum = new JFormattedTextField();
+		JLabel left_aLabel = new JLabel("Left Alone(%):");
+		JFormattedTextField lNum = new JFormattedTextField();
+		JLabel buildLabel = new JLabel("Building(%):");
+		JFormattedTextField bNum = new JFormattedTextField();
+		JLabel killLabel = new JLabel("Killing(%):");
+		JFormattedTextField kNum = new JFormattedTextField();
+		choices.add(governLabel);
+		choices.add(gNum);
+		choices.add(moneyLabel);
+		choices.add(mNum);
+		choices.add(left_aLabel);
+		choices.add(lNum);
+		choices.add(buildLabel);
+		choices.add(bNum);
+		choices.add(killLabel);
+		choices.add(kNum);
+		while(true){
+			try{
+				JOptionPane.showConfirmDialog(null, choices, "Choose % happiness for each:", JOptionPane.OK_CANCEL_OPTION);
+				gValue = Double.parseDouble(gNum.getText());
+				mValue = Double.parseDouble(mNum.getText());
+				lValue = Double.parseDouble(lNum.getText());
+				bValue = Double.parseDouble(bNum.getText());
+				kValue = Double.parseDouble(kNum.getText());
+				if((gValue + mValue + lValue + bValue + kValue) == 100) break;
+				else{
+					System.out.println("Not 100% assigned for Happiness");
+					JOptionPane.showMessageDialog(null, "Values must add up to 100.");
+				}
+			}
+			catch(NumberFormatException e){
+				System.out.println("Number Format error");
+				JOptionPane.showMessageDialog(null, "Invalid number entered.");
+			}
 		}
+		hap = new Happiness(gValue,mValue,lValue,bValue,kValue);
 	}
 }
